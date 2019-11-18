214eaed3344d7a8930a886fe7b4c6be6112af10f
abruzy <dialloabubakar5861@gmail.com>
2019-11-18 21:09:15 +0100

    linted

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 50 }
   validates :user_id, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 25 }
   validates :username, presence: true, uniqueness: true, length: { minimum: 4, maximum: 15 }
   before_save { self.email = email.downcase }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true, length: { maximum: 255 },
-              format: { with: VALID_EMAIL_REGEX },
-              uniqueness: { case_sensitive: false }
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 
   has_many :posts
   has_many :comments
