7c2aaa4905d29032cf4f876f397507e027fa19e2
abruzy <dialloabubakar5861@gmail.com>
2019-11-18 14:21:34 +0100

    post_id and user_id validation added

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
   validates :comment, presence: true, length: { minimum: 3, maximum: 20 }
+  validates :user_id, presence: true
+  validates :post_id, presence: true
 end
